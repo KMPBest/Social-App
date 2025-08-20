a71e5c22fb4d4777c77ae8c0e0b532a392c744f5
vapnguyen24 <vinhnt.work@gmail.com>
2025-08-20 10:59:47 +0700

    feat: enhance login screen UI and add social icons
    Updated the login screen with improved animations, visual enhancements, and replaced OutlinedTextField with TextField for email and password inputs. Added Google and Facebook icons for social login buttons, included new drawable resources, and updated .gitignore for Xcode-related files.

--- composeApp/src/commonMain/kotlin/org/edward/app/presentations/screens/auth/login/LoginScreen.kt
+++ composeApp/src/commonMain/kotlin/org/edward/app/presentations/screens/auth/login/LoginScreen.kt
@@ -1,5 +1,8 @@
 package org.edward.app.presentations.screens.auth.login
 
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -11,10 +14,14 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Key
 import androidx.compose.material.icons.filled.Visibility
 import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.material3.Button
@@ -24,30 +31,44 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.koin.koinScreenModel
 import cafe.adriel.voyager.navigator.LocalNavigator
 import cafe.adriel.voyager.navigator.currentOrThrow
+import kotlinx.coroutines.launch
+import multiplatform_app.composeapp.generated.resources.Res
+import multiplatform_app.composeapp.generated.resources.facebook
+import multiplatform_app.composeapp.generated.resources.google
+import multiplatform_app.composeapp.generated.resources.ic_cyclone
 import org.edward.app.presentations.navigations.RootAppDestination
 import org.edward.app.presentations.navigations.navigateTo
+import org.jetbrains.compose.resources.DrawableResource
+import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import org.koin.core.component.KoinComponent
 
@@ -64,44 +85,74 @@ class LoginScreen : Screen, KoinComponent {
 
         val navigator = LocalNavigator.currentOrThrow
 
-        val gradient = Brush.verticalGradient(colors = listOf(Color(0xFF4A00E0), Color(0xFF8E2DE2)))
+        Brush.verticalGradient(colors = listOf(Color(0xFF4A00E0), Color(0xFF8E2DE2)))
 
-        Column(modifier = Modifier.fillMaxSize().background(gradient).padding(top = 40.dp)) {
+        val scope = rememberCoroutineScope()
 
-            Row(
-                modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp).align(Alignment.End),
-                horizontalArrangement = Arrangement.End,
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Text("Don’t have an account?")
-                Spacer(modifier = Modifier.width(8.dp))
-                Text(
-                    "Get Started",
-                    fontWeight = FontWeight.Bold,
-                    modifier = Modifier
-                        .clip(RoundedCornerShape(12.dp))
-                        .clickable { }
-                        .padding(horizontal = 12.dp, vertical = 6.dp)
+        val alpha = remember { Animatable(0f) }
+        val scale = remember { Animatable(0.8f) }
+        val rotation = remember { Animatable(20f) }
+
+        val alphaText = remember { Animatable(0f) }
+        val offsetY = remember { Animatable(20f) }
+
+        LaunchedEffect(Unit) {
+            scope.launch {
+                alpha.animateTo(
+                    targetValue = 1f,
+                    animationSpec = tween(durationMillis = 400)
+                )
+            }
+            scope.launch {
+                scale.animateTo(
+                    targetValue = 1f,
+                    animationSpec = tween(durationMillis = 400)
+                )
+            }
+            scope.launch {
+                rotation.animateTo(
+                    targetValue = 0f,
+                    animationSpec = tween(durationMillis = 400)
                 )
             }
 
-            Spacer(modifier = Modifier.height(40.dp))
+            scope.launch {
+                alphaText.animateTo(
+                    targetValue = 1f,
+                    animationSpec = tween(600)
+                )
+            }
+            scope.launch {
+                offsetY.animateTo(
+                    targetValue = 0f,
+                    animationSpec = tween(700)
+                )
+            }
+        }
 
-            Text(
-                "Edward",
-                fontSize = 28.sp,
-                fontWeight = FontWeight.Bold,
-                modifier = Modifier.align(Alignment.CenterHorizontally)
-            )
+        Column(modifier = Modifier.fillMaxSize().padding(top = 40.dp)) {
+            Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
+                Image(
+                    painter = painterResource(Res.drawable.ic_cyclone),
+                    contentDescription = "",
+                    modifier = Modifier.size(100.dp)
+                        .scale(scale.value)
+                        .alpha(alpha.value)
+                        .rotate(rotation.value),
+                    colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.primary)
+                )
+            }
 
-            Spacer(modifier = Modifier.height(40.dp))
+            Spacer(modifier = Modifier.height(20.dp))
 
             Column(
                 modifier = Modifier
                     .fillMaxSize()
                     .clip(RoundedCornerShape(topStart = 32.dp, topEnd = 32.dp))
                     .background(MaterialTheme.colorScheme.background)
-                    .padding(24.dp),
+                    .padding(24.dp)
+                    .alpha(alphaText.value)
+                    .offset(y = offsetY.value.dp),
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
 
@@ -110,22 +161,28 @@ class LoginScreen : Screen, KoinComponent {
 
                 Spacer(modifier = Modifier.height(24.dp))
 
-                OutlinedTextField(
+                TextField(
                     value = uiState.email,
                     onValueChange = screenModel::onEmailChange,
+                    placeholder = { Text("Enter your email") },
                     label = { Text("Email Address") },
+                    modifier = Modifier.fillMaxWidth(),
                     singleLine = true,
-                    modifier = Modifier.fillMaxWidth()
+                    shape = RoundedCornerShape(8.dp),
+                    leadingIcon = { Icon(Icons.Default.Email, contentDescription = null) },
                 )
-
                 Spacer(modifier = Modifier.height(16.dp))
 
-                OutlinedTextField(
+                TextField(
                     value = uiState.password,
                     onValueChange = screenModel::onPasswordChange,
+//                    placeholder = { Text("Enter your password") },
                     label = { Text("Password") },
+                    modifier = Modifier.fillMaxWidth(),
                     singleLine = true,
+                    shape = RoundedCornerShape(8.dp),
                     visualTransformation = if (passwordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+                    leadingIcon = { Icon(Icons.Default.Key, contentDescription = null) },
                     trailingIcon = {
                         IconButton(onClick = { passwordVisible = !passwordVisible }) {
                             Icon(
@@ -134,7 +191,24 @@ class LoginScreen : Screen, KoinComponent {
                             )
                         }
                     },
-                    modifier = Modifier.fillMaxWidth()
+                )
+
+//                OutlinedTextField(
+//                    value = uiState.password,
+//                    onValueChange = screenModel::onPasswordChange,
+//                    label = { Text("Password") },
+//                    singleLine = true,
+//
+//                    modifier = Modifier.fillMaxWidth()
+//                )
+
+                Spacer(modifier = Modifier.height(12.dp))
+
+                Text(
+                    "Forgot your password?",
+                    fontSize = 14.sp,
+                    modifier = Modifier.fillMaxWidth().clickable { },
+                    textAlign = TextAlign.Right
                 )
 
                 Spacer(modifier = Modifier.height(24.dp))
@@ -161,13 +235,26 @@ class LoginScreen : Screen, KoinComponent {
                     }
                 }
 
-                Spacer(modifier = Modifier.height(12.dp))
+                Spacer(modifier = Modifier.height(8.dp))
 
-                Text(
-                    "Forgot your password?",
-                    fontSize = 14.sp,
-                    modifier = Modifier.clickable { }
-                )
+                Row(
+                    modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp),
+                    horizontalArrangement = Arrangement.Center,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text("Don’t have an account?")
+                    Text(
+                        "Register",
+                        fontWeight = FontWeight.Bold,
+                        modifier = Modifier
+                            .clip(RoundedCornerShape(12.dp))
+                            .clickable { }
+                            .padding(horizontal = 4.dp, vertical = 6.dp)
+                    )
+                }
+
+
+
 
                 Spacer(modifier = Modifier.height(24.dp))
 
@@ -190,24 +277,30 @@ class LoginScreen : Screen, KoinComponent {
 
                 Spacer(modifier = Modifier.height(16.dp))
 
-                Row(
-                    horizontalArrangement = Arrangement.SpaceEvenly,
-                    modifier = Modifier.fillMaxWidth()
+                Column(
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalArrangement = Arrangement.spacedBy(12.dp)
                 ) {
-                    SocialButton("Google")
-                    SocialButton("Facebook")
+                    SocialButton("Login With Google", Res.drawable.google)
+                    SocialButton("Login With Facebook", Res.drawable.facebook)
                 }
             }
         }
     }
 
     @Composable
-    fun SocialButton(platform: String) {
+    fun SocialButton(platform: String, resource: DrawableResource) {
         OutlinedButton(
             onClick = { },
-            modifier = Modifier.height(48.dp),
+            modifier = Modifier.height(48.dp).fillMaxWidth(),
             shape = RoundedCornerShape(12.dp)
         ) {
+            Image(
+                painter = painterResource(resource),
+                modifier = Modifier.size(36.dp),
+                contentDescription = "",
+            )
+            Spacer(Modifier.width(8.dp))
             Text(platform, fontSize = 14.sp)
         }
     }
